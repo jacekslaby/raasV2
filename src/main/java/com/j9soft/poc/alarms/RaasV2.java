48d89383d8fbdeb3eeb63eb4088c4e7d4a85b5d8
Jacek Slaby <jacek.slaby@interia.pl>
2018-09-06 09:19:30 +0100

    Added support for overwritten alarms. (i.e. updated ones)

--- src/main/java/com/j9soft/poc/alarms/RaasV2.java
+++ src/main/java/com/j9soft/poc/alarms/RaasV2.java
@@ -16,6 +16,11 @@ public interface RaasV2 {
      *
      * It may happen that an alarm is removed in between invocations of this method.
      * Such alarm is provided again in another pack and it contains null as its value.
+     * (So only the first pack is guaranteed not to contain null values.)
+     * (And bear in mind that any subsequent pack may contain an alarm already delivered in a previous pack -
+     * the only guarantee is that it does not happen within a pack.)
+     * This way clients are able to rebuild a consistent list of all alarms. (which may come and go while a client
+     * is in between retrievals of next packs.)
      *
      * If time between the beginning of retrieval of the first pack and the end of the last pack (the one with moreAvailable=false) is more than ONE hour
      * then it is possible that obsolete alarms (i.e. already removed) retrieved in the previous packs will not have "nullify" value in the following packs,
